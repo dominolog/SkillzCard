82ec497f604c86c4baad4bdd236f02cd8b7936fd
dominiktomczak <]WTxU\f2>
2018-02-08 15:58:55 +0100

    Changes

--- app/src/main/java/com/example/cubesoft/skillzcard/activity/PopupActivity.kt
+++ app/src/main/java/com/example/cubesoft/skillzcard/activity/PopupActivity.kt
@@ -4,32 +4,127 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import com.example.cubesoft.skillzcard.R
 import com.example.cubesoft.skillzcard.SkillzCardApplication
+import com.example.cubesoft.skillzcard.model.PopupDefinition
 import com.google.gson.Gson
 
 import kotlinx.android.synthetic.main.activity_popup.*
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.UnsupportedEncodingException
 
-class PopupActivity : AppCompatActivity() {
+class PopupActivity : AppCompatActivity(), OnItemSelectedListener {
+
+    private var popDef: PopupDefinition? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_popup)
         (application as SkillzCardApplication).getAppComponent().inject(this);
 
-
         val gson = Gson();
-
-        gson.fromJson<>()
-
+        popDef = gson.fromJson<com.example.cubesoft.skillzcard.model.PopupDefinition>(readIos(resources.assets.open("testPopup.json")),
+                PopupDefinition::class.java)
+        setupFields("EU");
 
         save.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            onSave();
+        }
+
+        unit.setOnItemSelectedListener  (this);
+
+    }
+
+    private fun onSave() {
+
+
+    }
+
+    override fun onItemSelected(view: AdapterView<*>, arg1: View, position: Int, id: Long) {
+        // use position to know the selected item
+        var aUnit = view.adapter.getItem(position);
+        setupFields(aUnit as String)
+
+    }
+
+    override fun onNothingSelected(arg0: AdapterView<*>) {
+
+    }
+
+    private fun setupFields(theUnit: String) {
+
+
+        inputs.removeAllViews();
+        for (field in popDef!!.fields) {
+
+            val child = LayoutInflater.from(this).inflate(R.layout.layout_input_field, inputs, false);
+            val input: TextInputEditText = child.findViewById<TextInputEditText>(R.id.input);
+
+
+            var hint = field.key;
+
+            val unit = findUnit(field.value.unit, popDef!!.units);
+            if ( unit != null ) {
+                val unitDefinition = findUnitDefinition(theUnit, unit.unitDefinition);
+                if (unitDefinition != null) {
+                    hint += " (" + unitDefinition.std + ")";
+                }
+            }
+
+            input.setHint(hint);
+
+
+            inputs.addView(child);
         }
     }
 
+    private fun findUnitDefinition(name: String, unitDefinitions: List<PopupDefinition.UnitDefinition>): PopupDefinition.UnitDefinition? {
+        for (unitDefinition in unitDefinitions) {
+            if (unitDefinition.name == name) {
+                return unitDefinition;
+            }
+        }
+        return null;
+    }
+
+    private fun findUnit(id: Int, units: List<PopupDefinition.Unit>): PopupDefinition.Unit? {
+        for (unit in units) {
+            if (unit.id == id) {
+                return unit;
+            }
+        }
+        return null;
+    }
+
+
+    fun readIos(input: InputStream): String {
+        val buffer = CharArray(1024)
+        val out = StringBuilder()
+        try {
+            InputStreamReader(input, "UTF-8").use({ input ->
+                while (true) {
+                    val rsz = input.read(buffer, 0, buffer.size)
+                    if (rsz < 0)
+                        break
+                    out.append(buffer, 0, rsz)
+                }
+            })
+        } catch (ex: UnsupportedEncodingException) {
+            /* ... */
+        } catch (ex: IOException) {
+            /* ... */
+        }
+
+        return out.toString()
+    }
 
     companion object {
         fun createIntent(context: Context) : Intent {
--- app/src/main/java/com/example/cubesoft/skillzcard/model/PopupDefinition.kt
+++ app/src/main/java/com/example/cubesoft/skillzcard/model/PopupDefinition.kt
@@ -6,29 +6,28 @@ import com.google.gson.annotations.SerializedName
  * Created by cube on 08.02.18.
  */
 
-class PopupDefinition {
+data class PopupDefinition(@SerializedName("fields") val fields: Map<String, Field>,
+                           @SerializedName("units") var units: List<Unit>) {
 
-    class Field {
-        @SerializedName("@unit")
-        var unit: Int;
+    class Field(@SerializedName("@unit")
+                val unit: Int) {
     }
 
-    class Unit {
-        @SerializedName("@id")
-        var id: Int;
-
-        @SerializedName("@description")
-        var description: String;
-
-        @SerializedName("@id")
-        var id: Int;
+    class Unit(@SerializedName("@id") val id: Int,
+               @SerializedName("@description") val description: String,
+               @SerializedName("unit-definition") val unitDefinition: List<UnitDefinition>) {
     }
 
-    @SerializedName("fields")
-    lateinit var fields: List<Field>;
 
-    @SerializedName("units")
-    lateinit var units: List<Unit>;
+    class UnitDefinition(@SerializedName("@name") val name: String,
+                         @SerializedName("@std") val std: String,
+                         @SerializedName("unit-conversion") val unitConversion: List<UnitConversion>) {
+    }
+
+    class UnitConversion(@SerializedName("@convToCM") val convToCM: String,
+               @SerializedName("@convFromCM") val convFromCM: String) {
+    }
+
 
 
 }
